b3fbe67f05dc4e4e869abfb9af3b1b5c018502ae
DevblueNotWorking <wooyeophan@gmail.com>
2021-03-09 14:37:37 +0900

    3월 9일 5교시 수업 코드

--- GamePrograming 3 9/GamePrograming 3 9.cpp
+++ GamePrograming 3 9/GamePrograming 3 9.cpp
@@ -3,15 +3,31 @@
 
 int main()
 {
+#pragma region 난수
     int randNum;
+    
+    time_t j = time(NULL);
+    printf("time(): %lld\r\n", j);
+
 
     srand((unsigned int)time(NULL));
+
 
     for (int i = 0; i < 10; ++i)
     {
         randNum = rand();
         printf("RandomNum: %d\r\n", randNum);
     }
+#pragma endregion
+
+#pragma region 시간
+    struct tm t = { 0 };
+    localtime_s(&t, &j);
+    printf("%d년 %d월 %d일", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday);
+#pragma endregion
+
+    
+
 
 
 

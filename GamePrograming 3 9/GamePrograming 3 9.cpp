14a53d1269c74024adf108636fe614a85a7dcdda
DevblueNotWorking <wooyeophan@gmail.com>
2021-03-09 14:23:59 +0900

    초기 커밋.

---
+++ GamePrograming 3 9/GamePrograming 3 9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+    int randNum;
+
+    srand((unsigned int)time(NULL));
+
+    for (int i = 0; i < 10; ++i)
+    {
+        randNum = rand();
+        printf("RandomNum: %d\r\n", randNum);
+    }
+
+
+
+    return(0);
+}

82125b96401a0cac530210852e2e71ac2e513385
Han at programing room <wooyeophan@gmail.com>
2021-03-23 16:23:03 +0900

    숫자맞추기 기반

--- GamePrograming 3 23/memoryGame.cpp
+++ GamePrograming 3 23/memoryGame.cpp
@@ -10,14 +10,24 @@ bool checkNumArchive(int*, int);
 
 void showNumbers(int*);
 void showHiddenNumbers();
+void userInput();
+
+void gotoXY(int x, int y);
 
 int main()
 {
 	int numbers[NUMBERS] = { 0, };
 
 	init(numbers);
-	//showNumbers(numbers);
+
+#pragma region BEGIN
+	showNumbers(numbers);
+	Sleep(3000);
+	system("cls");
 	showHiddenNumbers();
+#pragma endregion
+
+
 
 	return(0);
 }
@@ -29,12 +39,12 @@ void init(int* arr)
 
 	for (int i = 0; i < NUMBERS; ++i)
 	{
-		arr[i] = rand() % NUMBERS + 1;
+		arr[i] = rand() % NUMBERS / 2;
 		if (i > 1)
 		{
 			do
 			{
-				arr[i] = rand() % (NUMBERS / 2) + 1;
+				arr[i] = rand() % (NUMBERS / 2);
 			} while (!checkNumArchive(arr, i));
 		}
 	}
@@ -60,9 +70,8 @@ void showNumbers(int* arr)
 {
 	for (int i = 0; i < NUMBERS; ++i)
 	{
-		std::cout << arr[i] << "\t";
-		if (i % 5 == 4)
-			std::cout << std::endl;
+		gotoXY(i % 5, i / 5);
+		std::cout << arr[i];
 	}
 }
 
@@ -70,8 +79,62 @@ void showHiddenNumbers()
 {
 	for (int i = 0; i < NUMBERS; ++i)
 	{
-		std::cout << "бс" << "\t";
-		if (i % 5 == 4)
-			std::cout << std::endl;
+		gotoXY(i % 5, i / 5);
+		std::cout << '*';
 	}
+}
+
+void userInput(int* arr)
+{
+	int key;
+	int select[2] = { -1,-1 };
+	int selectCount = 0;
+
+	while (true)
+	{
+		key = _getch();
+		switch (key)
+		{
+		case 49:
+			gotoXY(0, 0);
+			arr[0];
+			select[selectCount] = arr[0];
+			++selectCount;
+			break;
+		case 50:
+			gotoXY(1, 0);
+			arr[1];
+			select[selectCount] = arr[1];
+			++selectCount;
+			break;
+		case 51:
+			gotoXY(2, 0);
+			arr[2];
+			select[selectCount] = arr[2];
+			++selectCount;
+			break;
+		}
+
+		Sleep(1000);
+
+		if (selectCount == 2)
+		{
+			if (select[0] != select[1])
+			{
+
+			}
+			selectCount = 0;
+		}
+	}
+}
+
+
+void gotoXY(int x, int y)
+{
+	HANDLE hOut;
+	COORD cur;
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	cur.X = x;
+	cur.Y = y;
+	SetConsoleCursorPosition(hOut, cur);
 }
